10d1ba36829c2d4beace42e431e2d1030f34b6bc
cmramsey3 <coltonramsey3@gmail.com>
2025-02-12 16:26:10 -0500

    Added comments to main.py

--- Hangman_Game/main_package/main.py
+++ Hangman_Game/main_package/main.py
@@ -2,7 +2,8 @@
 # Colton Ramsey
 # ramseyc6@mail.uc.edu
 
+# Play hangman with different difficulties and categories!
 from functions_package.functions import *
 
 if __name__ == "__main__":
-    play_hangman()
+    play_hangman() # runs the program
